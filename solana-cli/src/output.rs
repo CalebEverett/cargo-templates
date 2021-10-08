75a0218ae742cebcb6721553970facffbcd5b003
calebeverett <caleb@calebeverett.io>
2021-10-08 13:31:59 -0700

    initial commit

---
+++ solana-cli/src/output.rs
@@ -0,0 +1,39 @@
+use crate::config::Config;
+use console::Emoji;
+use serde::{Deserialize, Serialize};
+use solana_account_decoder::parse_token::UiTokenAmount;
+use solana_cli_output::{OutputFormat, QuietDisplay, VerboseDisplay};
+use std::fmt;
+
+static WARNING: Emoji = Emoji("⚠️", "!");
+
+pub(crate) fn println_display(config: &Config, message: String) {
+    match config.output_format {
+        OutputFormat::Display | OutputFormat::DisplayVerbose => {
+            println!("{}", message);
+        }
+        _ => {}
+    }
+}
+
+#[derive(Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub(crate) struct CliTokenAmount {
+    #[serde(flatten)]
+    pub(crate) amount: UiTokenAmount,
+}
+
+impl QuietDisplay for CliTokenAmount {}
+impl VerboseDisplay for CliTokenAmount {
+    fn write_str(&self, w: &mut dyn fmt::Write) -> fmt::Result {
+        writeln!(w, "ui amount: {}", self.amount.real_number_string_trimmed())?;
+        writeln!(w, "decimals: {}", self.amount.decimals)?;
+        writeln!(w, "amount: {}", self.amount.amount)
+    }
+}
+
+impl fmt::Display for CliTokenAmount {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        writeln!(f, "{}", self.amount.real_number_string_trimmed())
+    }
+}
